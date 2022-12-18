322111cca543ef39a751b02b849c133c9b349125
“[sanket]” <[sanket@gmail.com]>
2022-12-18 11:46:52 +0530

    tabrez code copy

---
+++ mysql-image/sql-scripts/InsertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('admin', 'admin', 'admin@gmail', 'admin', 'admin');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user1', 'user1', 'user1@gmail', 'user1', 'user1');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user2', 'user2', 'user2@gmail', 'user2', 'user2');
+INSERT INTO Employee (first_name, last_name, email, username, password) 
+VALUES ('user3', 'user3', 'user3@gmail', 'user3', 'user3');
